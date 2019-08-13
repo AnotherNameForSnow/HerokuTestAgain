83f9760c485b2b52e9866abb011364f0d65f7f3f
AnotherNameForSnow <zachpdmpt@gmail.com>
2019-08-13 09:31:30 -0400

    first commit

---
+++ src/main/java/com/tts/herokutestagain/HerokuTestAgainApplication.java
@@ -0,0 +1,13 @@
+package com.tts.herokutestagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTestAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestAgainApplication.class, args);
+	}
+
+}
